3ac12207d5d97a4be8d289b7a599b21324bbd2c2
Daniel P <danielpodvornyy@gmail.com>
2023-06-13 12:11:22 +0300

    init
    server side separated from client repo

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const server = express();
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connect();
+
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
+
+server.use(express.json());
+
+//Routes
+const userRouter = require("./routes/userRoutes");
+server.use("/users", userRouter);
+
+const stationRouter = require("./routes/stationRouters");
+server.use("/stations", stationRouter);
